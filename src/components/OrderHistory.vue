6f8deed480b3e0d20906efcfeb3b88c9f50d03ad
JesseStew <stewed.jesse@gmail.com>
2020-04-02 16:40:06 -0500

    package.json UserAccount OrderHistory

---
+++ src/components/OrderHistory.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="order-history">
+
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'OrderHistory',
+    props: {
+        orders: {
+            type: Object,
+            default: () => {
+                return {}
+            }
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/UserAccount.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="user-account">
+      <h1>User Account</h1>
+      <template v-if="!isLoading">
+
+      </template>
+      <!-- <h1>{{ user.name }}'s Account</h1> -->
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import OrderHistory from '../components/OrderHistory.vue'
+
+export default {
+    components: { OrderHistory },
+    data() {
+        return {
+            isLoading: true,
+            orders: []
+        }
+    },
+    created() {
+        axios.get('//localhost:3000/dashboard').then(({ data }) => {
+            this.orders = data.events.orders
+            this.isLoading = false
+        })
+    }
+}
+</script>
+
+<style>
+
+</style>
