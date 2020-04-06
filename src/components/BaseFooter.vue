5ec956a1e6a3a4805efed3c08b01cfd8b698cfe3
Jesse Stewart <JesseDStewartDev@gmail.com>
2020-04-05 22:56:38 -0500

    Home Page info added

---
+++ src/components/BaseFooter.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <p>
+      Copyright 2018 - 2019 All Rights Reserved. Selected content copyright and
+      trademark respective companies, used with permission.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped></style>
