6d04a619f95ee70d8ddfd92d3f07137ff14651ce
Jesse Stewart <JesseDStewartDev@gmail.com>
2020-04-20 15:47:05 -0500

    Added TestimonyItem component

---
+++ src/components/TestimonyItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div v-for="customer in customers" :key="customer.id">
+      {{ customer }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      customers: [],
+    }
+  },
+  firestore: {},
+}
+</script>
+
+<style scoped></style>
