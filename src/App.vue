b82b9932c6abd440383ebd5be487bb4e227dd1c6
Jesse Stewart <JesseDStewartDev@gmail.com>
2020-05-13 21:07:00 -0500

    Added footer

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,14 @@
   <v-app>
     <Navigation />
     <router-view />
+    <v-card height="150">
+      <v-footer absolute class="font-weight-medium">
+        <v-col class="text-center" cols="12">
+          Copyright 2018 - 2019 All Rights Reserved. Selected content copyright
+          and trademark respective companies, used with permission.
+        </v-col>
+      </v-footer>
+    </v-card>
   </v-app>
 </template>
 
