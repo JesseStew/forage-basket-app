60b1b24fe1bfb2922d6b8fcf46b25bdf9aa97156
Jesse Stewart <JesseDStewartDev@gmail.com>
2020-05-15 12:03:51 -0500

    Essential Oils content

--- src/views/EssentialOils.vue
+++ src/views/EssentialOils.vue
@@ -1,5 +1,127 @@
 <template>
   <div class="essential-oils">
-    <h1>This is the Essential Oils page</h1>
+    <v-content>
+      <v-row>
+        <v-col>
+          <h1>
+            Essential Oils
+          </h1>
+          <h2>
+            (Understanding the truth behind the industry.)
+          </h2>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <p>
+            Many people looking to improve their families health have discovered
+            essential oils. With so many essential oils options available, it
+            can be a task to understand what is really going on in the essential
+            oil and natural health supplement industries.
+          </p>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <img
+            src="https://firebasestorage.googleapis.com/v0/b/forage-basket-7a7ed.appspot.com/o/peppermint-compair.jpg?alt=media&token=1479a8a9-83d0-4922-ae9e-8cc9291e9b5e"
+            alt=""
+          />
+        </v-col>
+        <v-col>
+          <p>
+            ​There is a valid reason why two products with basically the exact
+            same labeling of Organic, Therapeutic Quality, and All Natural can
+            sell for $7.00 in one store and $20.00 in another.
+          </p>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          Below are some links to assist you in understanding the essential oils
+          industry and why we choose to sell the essential oils from the I Am
+          Essential Oils company.
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <!-- Add article -->
+          <ul>
+            <li>Article</li>
+            <li>Article</li>
+            <li>Article</li>
+            <li>Article</li>
+          </ul>
+        </v-col>
+        <v-col>
+          <!-- Add article -->
+          <ul>
+            <li>Article</li>
+            <li>Article</li>
+            <li>Article</li>
+            <li>Article</li>
+          </ul>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-btn>
+            Add Link
+          </v-btn>
+        </v-col>
+        <v-col>
+          <v-btn>
+            Add Link
+          </v-btn>
+        </v-col>
+      </v-row>
+      <v-row>
+        <h2>
+          I Am Essential Oils is Dedicated to Natural
+        </h2>
+        <p>
+          I Am Essential Oils, living naturally and in harmony with nature is
+          not only their passion, it is their way of life. The earth is abundant
+          and nature is our birthright. The health and well-being of our
+          community is directly correlated to our mutualistic relation with
+          nature. Nature provides clues to easy living, such as how bees
+          pollinate more efficiently than humans. At I Am Essential Oils they
+          guide their day to day living, work, and business relations for the
+          health of the planet.
+        </p>
+      </v-row>
+      <v-row>
+        <h2>
+          Dedicated to Fertile Soil &amp Healthier Living
+        </h2>
+        <p>
+          For the most part, nature cultivates and sustains better than man-made
+          systems. For example, the forest is a harmonious system which stores
+          water as if a lake and sustains life naturally. As forests are cut
+          down, the water that would have naturally been retained, now runs off
+          or is evaporated. This departure of water creates arid conditions and
+          leads to the loss of its vegetation and animal life. We can learn from
+          nature and apply what we experience to create better long term health
+          for the soil, and a healthier happier way of living for all living
+          things. I Am Essential Oils finds that the small farms are more
+          invested long term in soil quality.
+        </p>
+      </v-row>
+      <v-row>
+        <h2>
+          Dedicated to True Quality
+        </h2>
+        <p>
+          I Am Essential Oils is dedicated to providing true therapeutic oils
+          and herbal supplements. Truly therapeutic means their products are
+          sourced for reliable traditional/organic farms, unaltered, and pure;
+          therefore, they are at the strength to truly assist your family's
+          health. We specialize in working with farms that grow herbs in a truly
+          natural, harmonious, organic manner that not only provides the highest
+          healing value and sustainability, but also provides farmers the
+          ability to feed their families.
+        </p>
+      </v-row>
+    </v-content>
   </div>
 </template>
