eeb41e0d4256f4d3a753f750cce9e3d8e2cb6883
Jesse Stewart <JesseDStewartDev@gmail.com>
2020-06-01 18:48:55 -0500

    Create Stripe Customer

--- functions/index.js
+++ functions/index.js
@@ -6,11 +6,8 @@ admin.initializeApp()
 // const logging = require('@google-cloud/logging')()
 const stripe = require('stripe')(functions.config().stripe.token)
 
-exports.paymentIntent = functions.https.onRequest((request, response) => {
-  stripe.products.create(
-    {name: 'Gold Special'},
-    function(err, product) {
-      // asynchronously called
-    }
-  )
-}
+// When a user is created, register them with Stripe
+exports.createStripeCustomer = functions.auth.user().onCreate(async (user) => {
+  const customer = await stripe.customers.create({email: user.email});
+  return admin.firestore().collection('user').doc(user.uid).collection('stripe').doc('stripe_customer').set({stripe_customer_id: customer.id});
+});
