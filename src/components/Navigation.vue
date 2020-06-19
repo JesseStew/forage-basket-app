2257608dbbf62e0afe550e3de5da95671e72e2fb
Jesse Stewart <JesseDStewartDev@gmail.com>
2020-06-19 15:24:15 -0500

    Navigation

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,6 +1,6 @@
 <template>
   <nav style="height: 192px">
-    <v-app-bar prominent fixed color="blue-grey lighten-5">
+    <v-app-bar fixed color="blue-grey lighten-5">
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
       <v-spacer></v-spacer>
       <v-toolbar-title>Forage Basket</v-toolbar-title>
